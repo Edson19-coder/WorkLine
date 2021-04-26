a227b45ac176d2b65fcefc0e658f9566b5b3455d
edson.lugo <edson.lugo@netpay.com.mx>
2021-04-26 15:28:35 -0500

    Mensajes uno a uno sin preview del mensaje

--- WorkLine/app/src/main/java/com/example/workline/ChatActivity.kt
+++ WorkLine/app/src/main/java/com/example/workline/ChatActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.renderscript.Sampler
 import android.util.Log
+import com.example.workline.adapters.MessageAdapter
+import com.example.workline.adapters.MessageInChatAdapter
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.FirebaseDatabase
@@ -21,42 +23,56 @@ class ChatActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     private val dbrt = FirebaseDatabase.getInstance()
     private val mensajeriaRef = dbrt.getReference("Mensajeria")
+    private val listMessage = mutableListOf<Message>()
+    private val adapter = MessageInChatAdapter(listMessage)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
         auth = Firebase.auth
         var myUserId = auth.currentUser.uid
-        var friendUserId = "gd606cbMxWRP7Sxywbeb8S8sQfw2"
-
-        getMessages(myUserId, friendUserId)
+        //var friendUserId = "gd606cbMxWRP7Sxywbeb8S8sQfw2"
+        var friendUserId = "ECpsTvTvloeK2lCuwcKJOzlORpm2"
 
         btnSendMessage.setOnClickListener {
             val textMessage = editTextMessage.text.toString()
 
             if(myUserId.toString().isNotEmpty() && friendUserId.toString().isNotEmpty() && textMessage.isNotEmpty()) {
                 //Insertamos el mensaje en el usuario emisor
-                insertMessage(myUserId.toString(), friendUserId, textMessage)
+                insertMessage(myUserId.toString(), friendUserId, textMessage, myUserId.toString())
                 //Insertamos el mensaje en el usuario remitente
-                insertMessage(friendUserId, myUserId.toString(), textMessage)
+                insertMessage(friendUserId, myUserId.toString(), textMessage, myUserId.toString())
             }
             editTextMessage.text.clear()
         }
+        rvChat.adapter = adapter
+        getMessages(myUserId, friendUserId)
     }
 
-    private fun insertMessage(emitter: String, remmiter: String, textMessage: String) {
+    private fun insertMessage(me: String, friend: String, textMessage: String, emitter: String) {
         val sdf = SimpleDateFormat("dd/M/yyyy hh:mm:ss")
         val created_at = sdf.format(Date())
         val message = Message(mensajeriaRef.push().key.toString(), textMessage, emitter.toString(), created_at)
-        mensajeriaRef.child(emitter).child(remmiter).child(message.id).setValue(message)
+        mensajeriaRef.child(me).child(friend).child(message.id).setValue(message)
     }
 
     private fun getMessages(userId: String ,friendId: String) {
         mensajeriaRef.child(userId).child(friendId).addValueEventListener(object : ValueEventListener {
+
             override fun onDataChange(snapshot: DataSnapshot) {
+                listMessage.clear()
                 for(snap in snapshot.children) {
-                    val message = snap
+                    val message: Message = snap.getValue(
+                            Message::class.java
+                    ) as Message
+                    message.mine = message.emitter.equals(userId)
+                    //message.mine = message.emitter == userId
+                    listMessage.add(message)
                     Log.d("Success", message.toString())
+                }
+                if(listMessage.size > 0 && listMessage != null) {
+                    adapter.notifyDataSetChanged()
+                    rvChat.smoothScrollToPosition(listMessage.size - 1)
                 }
             }
 
@@ -65,5 +81,6 @@ class ChatActivity : AppCompatActivity() {
             }
 
         })
+        adapter.notifyDataSetChanged()
     }
 }
--- WorkLine/app/src/main/java/com/example/workline/modelos/Message.kt
+++ WorkLine/app/src/main/java/com/example/workline/modelos/Message.kt
@@ -1,9 +1,13 @@
 package com.example.workline.modelos
 
+import com.google.firebase.database.Exclude
+
 class Message(
     var id: String = "",
     var content: String = "",
     var emitter: String = "",
     var created_at: String = ""
 ) {
+    @Exclude
+    var mine: Boolean = false
 }
