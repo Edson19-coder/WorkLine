c8ccae958c6c53d330f12415bf73cc17d4526216
DavidMontemayorVeliz <david.montemayor@netpay.com.mx>
2021-04-26 23:05:47 -0500

    Agregado get-set de Activity a Fragment

--- WorkLine/app/src/main/java/com/example/workline/HomeActivity.kt
+++ WorkLine/app/src/main/java/com/example/workline/HomeActivity.kt
@@ -13,14 +13,20 @@ import androidx.navigation.navArgs
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.example.workline.fragments.MessagesFragment
+import com.example.workline.modelos.User
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.oAuthCredential
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.activity_login.*
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private val db = FirebaseFirestore.getInstance()
 
-    public var email:String = "";
+    var email:String = ""
+    var correoA = "davidmove@mail.com" //User.correo
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,13 +39,25 @@ class HomeActivity : AppCompatActivity() {
         val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
         prefs.putString("email", email)
 
+        setCorreo("edson@mail.com")
         val appBarConfiguration = AppBarConfiguration(setOf(R.id.messagesFragment, R.id.groupsFragment, R.id.tasksFragment))
         bottomNavigationView.setupWithNavController(findNavController(R.id.fragment))
+
     }
 
     private fun showStart() {
         val activityMain = Intent(this, MainActivity::class.java)
         startActivity(activityMain)
+    }
+
+    @JvmName("getCorreo1")
+    fun getCorreo(): String {
+        return correoA
+    }
+
+    @JvmName("setCorreo1")
+    fun setCorreo(correo:String) {
+        this.correoA = correo
     }
 
 }
--- WorkLine/app/src/main/java/com/example/workline/fragments/GroupsFragment.kt
+++ WorkLine/app/src/main/java/com/example/workline/fragments/GroupsFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.example.workline.HomeActivity
 import com.example.workline.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -18,15 +20,11 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class GroupsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    var correoF = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
         }
     }
 
@@ -34,6 +32,13 @@ class GroupsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        correoF = (getActivity() as HomeActivity).getCorreo()
+
+        val duration = Toast.LENGTH_SHORT
+        val toast = Toast.makeText(context, correoF, duration)
+        toast.show()
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_groups, container, false)
     }
